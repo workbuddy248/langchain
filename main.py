e7dd327ce99fdacac3d77fc757dcffdfd4907f80
workbuddy <workbuddy248@gmail.com>
2026-03-16 12:55:10 +0530

    search-agent

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
+
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
+from langchain.agents.structured_output import ToolStrategy
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
-#from tavily import TavilyClient
 from langchain_tavily import TavilySearch
 
-#tavily = TavilyClient()   
+class Source(BaseModel):
+    """Schema of a source used by Agent"""
+    url: str = Field(description="The URL of the source")
 
-#@tool()
-#def search(query: str) -> str:
-#    """
-#    Tool to search the web for information.
-#    Args:        
-#        query (str): The search query.
-#    Returns:        
-#        str: The search results.
-#    """
-#    print(f"Searching for: {query}")
-#    return tavily.search(query=query)
+class AgentResponse(BaseModel):
+    """Schema of the Agent response"""
+    answer: str = Field(description="The agents answer to the user's query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
 
-
+  
 # Initialize the Ollama language model
 llm = ChatOllama(model="gpt-oss:20b")
     
 # Create an agent with the language model and the search tool
 tool = [TavilySearch()]  # Using the TavilySearch tool instead of the custom search function]
-agent = create_agent(model=llm, tools=tool)
+agent = create_agent(
+    model=llm,
+    tools=tool,
+    response_format=ToolStrategy(AgentResponse),
+)
 
 def main():  
-    # Example query to the agent
     print("welcome to Search agent")
-    query = "search for 3 job postings for an ai engineer using langchain in India on linkedin and list their details"
-    result = agent.invoke({"messages":HumanMessage(content=query)})   
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="search for the latest news about AI and provide the sources"
+            )
+        }
+    )   
     print(result)
 
 if __name__ == "__main__":   
